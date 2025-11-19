ab507af8e7c3b63b02d1f109711e2b2668b2bc31
Enzo Mendes <aguiarenzo895@gmail.com>
2025-11-19 16:36:26 -0300

    eee

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,13 +1,21 @@
-const seletorCor=document.getElementById('seletorCor');
-const btnAplicarCor=document.getElementById('btnAplicarCor');
+const seletorCor = document.getElementById("seletorCor");
+const btnAplicarCor = document.getElementById("btnAplicarCor");
 
-btnAplicarCor.addEventListener("click",(e)=>{
-    const cor =seletorCor.value;
+btnAplicarCor.addEventListener("click", (e) => {
+    const cor = seletorCor.value;
     document.documentElement.style.setProperty("--cor-fundo", cor);
 });
 
 const chips = document.querySelectorAll(".chip");
-const galeria= document.getElementById("galeriaHero");
+const galeria = document.getElementById("galeriaHero");
 
-const imagemInical = chips[0].dataset.imagem;
-galeria.style.backgroundImage =`url()`
+const imagemInicial = chips[0].dataset.imagem;
+galeria.style.backgroundImage = `url(${imagemInicial})`
+
+// forEach: para cada objeto (repetir)
+chips.forEach((chip) => {
+    chip.addEventListener("click", () => {
+        galeria.classList.add("ativa");
+        galeria.style.backgroundImage = `url(${chip.dataset.image})`;
+    })
+})
